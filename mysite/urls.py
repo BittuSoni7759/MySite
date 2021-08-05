15a7253db6aaa51cb2b32e7d6e2babebd16ab8a1
Bittu Kumar <bittusoni302@gmail.com>
2021-08-05 11:08:26 +0530

    update views

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,9 +29,10 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('removepunc',views.removepunc,name='rempun'),
-    path('capitalizefirst',views.capfirst,name='capfirst'),
-    path('newlineremove',views.newlineremove,name='newlineremove'),
-    path('spaceremove',views.spaceremove,name='spaceremove'),
-     path('charcount',views.charcount,name='charcount'),
+    path('analyze',views.removepunc,name='rempun'),
+    # path('removepunc',views.removepunc,name='rempun'),
+    # path('capitalizefirst',views.capfirst,name='capfirst'),
+    # path('newlineremove',views.newlineremove,name='newlineremove'),
+    # path('spaceremove',views.spaceremove,name='spaceremove'),
+    #  path('charcount',views.charcount,name='charcount'),
 ]
